f2fc343198f4b7da15396cf2bb35a83206b0e4f7
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-06-26 09:16:35 +0200

    Kicking it off

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject me.mishok13/bktree "0.1.0-SNAPSHOT"
+  :description "BK-tree implementation"
+  :url "http://github.com/mishok13/bktree"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
