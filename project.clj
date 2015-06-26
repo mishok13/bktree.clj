ac967ebbe07408ea6f43386893382b969f1ea2d7
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-06-26 09:28:21 +0200

    License questions

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject me.mishok13/bktree "0.1.0-SNAPSHOT"
   :description "BK-tree implementation"
   :url "http://github.com/mishok13/bktree"
-  :license {:name "MIT"}
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]]}})
