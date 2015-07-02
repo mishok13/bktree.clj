39a179061dfe1a42d28c97ad2b8527971217eaaa
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-07-02 11:25:44 +0200

    Decrease number of tested words to 10

--- benchmark/benchmark.clj
+++ benchmark/benchmark.clj
@@ -22,7 +22,7 @@
          (map (juxt identity (partial choose-edit-distance rand-gen))))))
 
 (defn bk-tree
-  [dictionary]
+  [dictionary words]
   (println "Running BK-tree benchmark")
   ;; Perhaps optimize decision making by randomizing list first? That
   ;; would introduce some unpredictability factor though, so hmmm...
@@ -30,7 +30,7 @@
   (let [tree (reduce bktree/insert (bktree/make-tree) dictionary)]
     (println "All ready to go")
     (bench/bench
-     (doseq [[word edit-distance] (take 25 (choose-words dictionary))]
+     (doseq [[word edit-distance] words]
        (bktree/search tree word edit-distance)))))
 
 (defn spellchecker
@@ -39,18 +39,19 @@
    (Thread/sleep 1000)))
 
 (defn simple-list
-  [dictionary]
+  [dictionary words]
   (println "Running simple list benchmark")
   (bench/bench
-   (doseq [[word edit-distance] (take 25 (choose-words dictionary))]
+   (doseq [[word edit-distance] words]
      (->> dictionary
           (filter (fn [entry] (<= (bktree/levenstein-distance entry word) edit-distance)))
           doall))))
 
 (defn -main
   [benchmark]
-  (let [dictionary (doall (map str/lower-case (line-seq (io/reader "/usr/share/dict/words"))))]
+  (let [dictionary (doall (map str/lower-case (line-seq (io/reader "/usr/share/dict/words"))))
+        words (take 10 (choose-words dictionary))]
     (case benchmark
-      "bktree" (bk-tree dictionary)
-      "spellchecker" (spellchecker)
-      "simple" (simple-list dictionary))))
+      "bktree" (bk-tree dictionary words)
+      "spellchecker" (spellchecker dictionary)
+      "simple" (simple-list dictionary words))))
